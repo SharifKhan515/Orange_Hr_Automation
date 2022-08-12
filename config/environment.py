aa6195e623ef59ca8353199c69bf4f597096787f
SharifKhan515 <sharifkhan515@gmail.com>
2022-08-12 21:27:54 +0600

    1. Add assertion message for login test

--- config/environment.py
+++ config/environment.py
@@ -4,8 +4,8 @@ class Environment:
     BASE_URL = "https://opensource-demo.orangehrmlive.com/"
     USER_NAME = "Admin"
     PASSWORD = "admin123"
-    # BASE_URL = "https://freesharif-osondemand.orangehrm.com/"
-    # PASSWORD = "@qPJ@X7@2gPp"
+    #BASE_URL = "https://freesharif-osondemand.orangehrm.com/"
+    #PASSWORD = "@qPJ@X7@2gPp"
     IMPLICIT_WAIT = 10
     EXPLICIT_WAIT = 10
     FIREFOX_LOG_PATH = None
